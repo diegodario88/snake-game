87a15e065207803e7a6519f6e0ad50d953a0c91f
Diego Dario <diegodario88@gmail.com>
2020-06-02 19:52:40 -0300

    feat: added input feature

--- config.js
+++ config.js
@@ -1,19 +1,11 @@
-export const SNAKE_SPEED = 1
-const snakeBody = [{
-        x: 10,
-        y: 11
-    },
-    {
-        x: 11,
-        y: 11
-    },
-    {
-        x: 12,
-        y: 11
-    }
-]
+import { getInputDirections } from "./input.js"
+
+export const SNAKE_SPEED = 5
+const snakeBody = [{x: 11, y: 11}]
+
 
 export function update() {
+    const inputDirection = getInputDirections()
     for (let i = snakeBody.length - 2; i >= 0; i--) {
         snakeBody[i + 1] = {
             ...snakeBody[i]
@@ -21,8 +13,8 @@ export function update() {
 
     }
 
-    // snakeBody[0].x += 1
-    // snakeBody[0].y += 0
+     snakeBody[0].x += inputDirection.x
+     snakeBody[0].y += inputDirection.y
 }
 
 export function draw(gameBoard) {
