b4ac7f92b8220137a9c0f12ff036c2de571e5fea
Diego Dario <diegodario88@gmail.com>
2020-06-04 11:14:37 -0300

    feat: added vim movement keys

--- input.js
+++ input.js
@@ -3,27 +3,19 @@ let lastInputDirection = { x: 0, y: 0 }
 
 window.addEventListener("keydown", e => {
     switch (e.key) {
-        case 'ArrowUp':
-            if (lastInputDirection.y !== 0) break
-            inputDirections = { x: 0, y: -1 }
+        case 'k': case 'ArrowUp':
+            lastInputDirection.y !== 0 ? null : inputDirections = { x: 0, y: -1 }
             break;
-        case 'ArrowDown':
-            if (lastInputDirection.y !== 0) break
-            inputDirections = { x: 0, y: 1 }
+        case 'j': case 'ArrowDown':
+            lastInputDirection.y !== 0 ? null : inputDirections = { x: 0, y: 1 }
+            break
+            case 'a': case 'h': case 'ArrowLeft':
+            lastInputDirection.x !== 0 ? null : inputDirections = { x: -1, y: 0 }
             break;
-        case 'ArrowLeft':
-            if (lastInputDirection.x !== 0) break
-            inputDirections = { x: -1, y: 0 }
-            break;
-        case 'ArrowRight':
-            if (lastInputDirection.x !== 0) break
-            inputDirections = { x: 1, y: 0 }
-            break;
-
-        default:
+        case 'l': case 'ArrowRight':
+            lastInputDirection.x !== 0 ? null : inputDirections = { x: 1, y: 0 }
             break;
     }
-
 })
 
 export function getInputDirections() {
